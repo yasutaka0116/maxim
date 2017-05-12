8f6b8d288a6007e9a8efcdb168124cde89cb8acc
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2017-05-12 17:09:27 +0900

    maxims

--- lib/maxim.rb
+++
@@ -1,9 +0,0 @@
-require "maxim/version"
-
-module Maxim
-  # Your code goes here...
-  def self.tired
-    word = ["Think rich, look poor.","When you come to a roadblock, take a detour.","A problem is your chance to do your best.","One of these days is none of these days.","Believe and act as if it were impossible to fail.", "I walk slowly, but I never walk backward.", "There is more to life than increasing its speed.","If you can dream it, you can do it.","Growth is often a painful process.","Do one thing everyday that scares you."," Conquer yourself rather than the world.","It always seems impossible until it’s done.","Always bear in mind that your own resolution to succeed is more important than any one thing."," In the middle of difficulty lies opportunity.", "Imagination means nothing without doing.", "Patience is also a form of action.", "Doubt is the origin of wisdom.", "Saying is one thing and doing is another.", "Quality means doing it right when no one is looking.", "Your time is limited, so don’t waste it living someone else’s life.","Stay hungry. Stay foolish.", " The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle.", " Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful."]
-     return word.sample
-  end
-end
--- lib/maxim/version.rb
+++
@@ -1,3 +0,0 @@
-module Maxim
-  VERSION = "0.1.0"
-end
